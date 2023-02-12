14b44dc42336fa161b99a1af1d64b3557717f4d4
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-02-12 18:47:13 +0545

    added necessary codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:foodit/core/responsive/screen_util.dart';
+import 'package:foodit/core/routes/route_generator.dart';
 import 'package:foodit/core/theme/app_theme.dart';
 import 'package:foodit/modules/auth/login/view/login_screen.dart';
 import 'package:foodit/widgets/password_field/password_provider.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.transparent, // navigation bar color
+
+    statusBarColor: Colors.transparent, // status bar color
+  ));
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(const MyApp());
 }
 
@@ -14,7 +24,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init();
+    ScreenUtil.init(height: 844, width: 390, allowFontScaling: true);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
@@ -26,6 +36,7 @@ class MyApp extends StatelessWidget {
         title: 'FoodIt',
         theme: AppTheme.lightTheme,
         home: const LoginScreen(),
+        onGenerateRoute: (settings) => RouteGenerator.getRoute(settings),
       ),
     );
   }
