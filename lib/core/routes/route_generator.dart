4bf4c381521c54798f5dc74b12248cc4da308198
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-02-12 18:47:22 +0545

    created route generator

--- lib/core/routes/route_generator.dart
+++ lib/core/routes/route_generator.dart
@@ -1,1 +1,28 @@
-class RouteGenerator {}
+import 'package:flutter/material.dart';
+import 'package:foodit/core/routes/routes.dart';
+import 'package:foodit/modules/auth/login/view/login_screen.dart';
+import 'package:foodit/modules/auth/register/view/register_screen.dart';
+import 'package:foodit/modules/homescreen/view/homescreen.dart';
+
+class RouteGenerator {
+  RouteGenerator._();
+
+  static Route<dynamic> getRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case Routes.login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case Routes.register:
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+
+      case Routes.homescreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
+
+      default:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+    }
+  }
+}
