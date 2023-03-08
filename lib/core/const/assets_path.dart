7aad8fcb9a5099924317a96daeec414e912fb651
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-09 00:18:07 +0545

    new assets added

--- lib/core/const/assets_path.dart
+++ lib/core/const/assets_path.dart
@@ -6,4 +6,16 @@ class AssetPaths {
 
   static const String googleLogo = "$_iconPath/google.png";
   static const String bg = "$_imagePath/bg.jpg";
+  static const String hookah = "$_imagePath/hookah.jpg";
+  static const String hotdog = "$_imagePath/hotdog.jpg";
+  static const String jholMomo = "$_imagePath/jhol-momo.jpg";
+  static const String cold = "$_imagePath/cold.jpg";
+  static const String coldDrink = "$_imagePath/cold-drink.jpg";
+  static const String frenchFries = "$_imagePath/french-fries.jpg";
+  static const String paneerTikka = "$_imagePath/paneer-tikka.jpg";
+  static const String rice = "$_imagePath/rice.jpg";
+  static const String burger = "$_imagePath/burger.jpg";
+  static const String hotCoffee = "$_imagePath/hot-coffee.jpeg";
+  static const String friedChicken = "$_imagePath/fried-chicken.png";
+  static const String momo = "$_imagePath/momo.png";
 }
