ad0e5a815e5bb6e673f4b7b95eed79b138d54e93
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-10 22:08:07 +0545

    added on tap and on fav to home provider

--- lib/modules/homescreen/provider/home_provider.dart
+++ lib/modules/homescreen/provider/home_provider.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:foodit/core/const/assets_path.dart';
+import 'package:foodit/modules/search_screen/search_screen.dart';
 
 import '../../../data/models/category.dart';
 
 class HomeProvider with ChangeNotifier {
+  int selectedIndex = 0;
+  onNavTap(BuildContext context, int index) {
+    if (index == 1) {
+      showSearch(context: context, delegate: SearchBarDelegate());
+    }
+    selectedIndex = index;
+    notifyListeners();
+  }
+
   List<Category> categoryList = List.generate(
     5,
     (index) => Category(
         id: index, imgPath: AssetPaths.frenchFries, name: "French Fries"),
   );
+
+  bool isFav = false;
+  void onFav(bool value) {
+    isFav = !value;
+    notifyListeners();
+  }
 }
