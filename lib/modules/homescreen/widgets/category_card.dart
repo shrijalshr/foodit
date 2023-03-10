797aef27f116b7a055f66a23d23939404794ce33
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-10 22:08:53 +0545

    wrapped cat card with gesture detector

--- lib/modules/homescreen/widgets/category_card.dart
+++ lib/modules/homescreen/widgets/category_card.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:foodit/core/extensions/app_extensions.dart';
+import 'package:foodit/core/routes/routes.dart';
 
 class CategoryCard extends StatelessWidget {
   const CategoryCard({
@@ -16,38 +16,43 @@ class CategoryCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(8),
-      child: Container(
-        width: 100,
-        decoration: BoxDecoration(
-          color: context.color.white,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        child: Column(
-          children: [
-            SizedBox(
-              width: 100,
-              height: 80,
-              child: Image.asset(
-                imgPath,
+    return GestureDetector(
+      onTap: () {
+        Navigator.pushNamed(context, Routes.itemList);
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Container(
+          width: 100,
+          decoration: BoxDecoration(
+            color: context.color.white,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
                 width: 100,
                 height: 80,
-                fit: BoxFit.cover,
-              ).pb(8),
-            ),
-            SizedBox(
-              height: 15,
-              child: Text(
-                label,
-                textAlign: TextAlign.center,
-                style: context.textTheme.displayMedium
-                    ?.copyWith(color: context.color.lightGrey),
-                maxLines: 2,
-                overflow: TextOverflow.clip,
+                child: Image.asset(
+                  imgPath,
+                  width: 100,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ).pb(8),
               ),
-            ),
-          ],
+              SizedBox(
+                height: 15,
+                child: Text(
+                  label,
+                  textAlign: TextAlign.center,
+                  style: context.textTheme.displayMedium
+                      ?.copyWith(color: context.color.lightGrey),
+                  maxLines: 2,
+                  overflow: TextOverflow.clip,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
