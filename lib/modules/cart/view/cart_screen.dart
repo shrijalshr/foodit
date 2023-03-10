f7d8d3daa1d443c914bf0ac4eac604419c3840ec
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-11 00:30:42 +0545

    refactored and ui fixes

--- lib/modules/cart/view/cart_screen.dart
+++ lib/modules/cart/view/cart_screen.dart
@@ -1,52 +1,116 @@
 import 'package:flutter/material.dart';
-import 'package:foodit/core/const/assets_path.dart';
-import 'package:foodit/core/extensions/app_extensions.dart';
+import 'package:foodit/core/export.dart';
+import 'package:foodit/widgets/app_button.dart';
 import 'package:foodit/widgets/dash_container.dart';
+import '../widget/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
-
+  const CartScreen({super.key, this.showAppBar = false});
+  final bool showAppBar;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "My Cart",
-        ),
-      ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            DashContainer(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8),
-                      child: Image.asset(
-                        AssetPaths.jholMomo,
-                        height: 30,
-                        width: 30,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                      child: Column(
-                    children: [
-                      Text(
-                        "Chicken Jhol Momo",
-                        style: context.textTheme.displayLarge,
-                      ),
-                    ],
-                  ))
-                ],
+      appBar: showAppBar
+          ? AppBar(
+              title: const Text(
+                "My Cart",
               ),
             )
-          ],
-        ),
-      ),
+          : null,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            constraints: BoxConstraints(
+                maxHeight: 40.fh,
+                minHeight: 10.fh,
+                maxWidth: 100.fw,
+                minWidth: 100.fw),
+            child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) => CartItemTile(
+                      id: index,
+                      name: "Chicken Jhol Momo",
+                      category: "Nepali Food",
+                      price: "250",
+                      imgPath: AssetPaths.jholMomo,
+                    ),
+                separatorBuilder: (context, index) => const SizedBox(
+                      height: 10,
+                    ),
+                itemCount: 2),
+          ),
+          Expanded(
+            child: DashContainer(
+              child: SingleChildScrollView(
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Sub total",
+                          style: context.textTheme.displayMedium,
+                        ),
+                        Text(
+                          "Rs. 750",
+                          style: context.textTheme.displayMedium,
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Discount",
+                          style: context.textTheme.displayMedium,
+                        ),
+                        Text(
+                          "Rs. 0",
+                          style: context.textTheme.displayMedium,
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Delivery",
+                          style: context.textTheme.displayMedium,
+                        ),
+                        Text(
+                          "Rs. 0",
+                          style: context.textTheme.displayMedium,
+                        ),
+                      ],
+                    ),
+                    const Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Total",
+                          style: context.textTheme.displayMedium,
+                        ),
+                        Text(
+                          "Rs. 750",
+                          style: context.textTheme.displayMedium,
+                        ),
+                      ],
+                    ).pb(16),
+                    AppButton(onPressed: () {}, label: "Proceed To Checkout")
+                  ],
+                ).pa(16),
+              ),
+            ).pt(10),
+          ),
+        ],
+      ).ph(16),
     );
   }
 }
