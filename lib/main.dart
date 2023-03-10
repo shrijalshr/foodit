3d758ed3c65f2d13e56498babc6c49e256613740
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-11 00:29:46 +0545

    added necessary providers

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,14 @@ import 'package:foodit/core/responsive/screen_util.dart';
 import 'package:foodit/core/routes/route_generator.dart';
 import 'package:foodit/core/theme/app_theme.dart';
 import 'package:foodit/modules/auth/login/view/login_screen.dart';
+import 'package:foodit/modules/cart/provider/cart_provider.dart';
 import 'package:foodit/modules/homescreen/provider/home_provider.dart';
 import 'package:foodit/modules/item_detail/provider/item_detail_provider.dart';
+import 'package:foodit/modules/my_order/provider/my_order_provider.dart';
 import 'package:foodit/widgets/password_field/password_provider.dart';
 import 'package:provider/provider.dart';
+
+import 'modules/cart/widget/cart_item.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -37,6 +41,15 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => HomeProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CartProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CartItemProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => MyOrderProvider(),
         ),
       ],
       child: MaterialApp(
