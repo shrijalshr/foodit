e27a53ed650df0a0f39f2b2a8ee73f9bd4b077f9
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-03-11 01:07:16 +0545

    created fav screen

---
+++ lib/modules/favorite/view/favorite_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../item_list/view/item_list.dart';
+
+class FavoriteScreen extends StatelessWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Favorites",
+        ),
+      ),
+      body: GridView.builder(
+        scrollDirection: Axis.vertical,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 19,
+        ),
+        physics: const BouncingScrollPhysics(),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ItemCard(
+            id: index,
+            name: "Chicken Jhol Momo",
+            price: "250",
+          );
+        },
+      ),
+    );
+  }
+}
