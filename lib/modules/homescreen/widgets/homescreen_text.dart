ce826cabff2eb9ffcf76c052dbedf14d18b33cbc
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-04-10 23:14:34 +0545

    moved homescreen text

---
+++ lib/modules/homescreen/widgets/homescreen_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:foodit/core/export.dart';
+
+class HomeScreenText extends StatelessWidget {
+  const HomeScreenText({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "What would you like to have today?",
+      style: context.textTheme.headlineLarge,
+    );
+  }
+}
