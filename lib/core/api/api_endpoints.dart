c1ba31036cacaac1f8670b89eb655f2cd0dabe0f
Shrijal Shrestha <shrijalshrestha313@gmail.com>
2023-04-21 20:07:13 +0545

    added api endpoint class

---
+++ lib/core/api/api_endpoints.dart
@@ -0,0 +1,10 @@
+class ApiEndPoints {
+  ApiEndPoints._();
+
+  static const BASE_URL = "http://192.168.1.152:8001/api/";
+  static const IMG_URL = "http://192.168.1.152:8001/";
+
+  ///
+  static const categories = "cats";
+  static const items = "items";
+}
